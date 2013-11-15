c57dcd26eb9ebd5ec575f7c31599a7573968f036
Lucas <lucas@dominiol.com.br>
2013-11-15 03:11:05 -0200

    Fatoração.

--- fontes/js/nodoWeb.js
+++ fontes/js/nodoWeb.js
@@ -2,28 +2,74 @@ var http = require("http");
 var sistemaDeArquivos = require("fs");
 var url = require("url");
 
-http.createServer(function (requisicao, resposta) {
+function responderRequisicao(requisicao, resposta) {
 	var metodo = requisicao.method;
 	if (metodo === "GET") {
-		var uri = url.parse(requisicao.url, true);
-		var caminho = "." + uri.pathname;
-		sistemaDeArquivos.exists(caminho, function (existe) {
-			if (existe) {
-				sistemaDeArquivos.stat(caminho, function (erro, estado) {
-					if (!erro && estado.isFile()) {
-						responderComoArquivo(resposta, caminho);
-					} else {
-						responderComoNaoEncontrado(resposta, caminho);
-					}
-				});
-			} else {
-				responderComoNaoEncontrado(resposta, caminho);
-			}
-		});
+		responderGet(requisicao, resposta);
 	} else {
-		responderComoMetodoNaoPermitido(resposta);
+		responder405(resposta, metodo);
 	}
-}).listen(7000);
+}
+
+function responderGet(requisicao, resposta) {
+	var uri = url.parse(requisicao.url, true);
+	var caminho = ("." + uri.pathname);
+	sistemaDeArquivos.exists(caminho, function (existe) {
+		if (existe) {
+			responderRecurso(resposta, caminho);
+		} else {
+			responder404(resposta, caminho);
+		}
+	});
+}
+
+function responderRecurso(resposta, caminho) {
+	sistemaDeArquivos.stat(caminho, function (erro, estado) {
+		if (!erro && estado.isFile()) {
+			responderArquivo(resposta, caminho);
+		} else {
+			responder404(resposta, caminho);
+		}
+	});
+}
+
+function responderArquivo(resposta, caminho) {
+	sistemaDeArquivos.readFile(caminho, function (erro, arquivo) {
+		if (erro) {
+			responder500();
+		} else {
+			var extensao = caminho.split(".").pop();
+			var tipoDeMidia = tiposDeMidiaPorExtensao[extensao];
+			if (tipoDeMidia === null) {
+				tipoDeMidia = "text/plain";
+			}
+			responder200(resposta, arquivo, tipoDeMidia);
+		}
+	});
+}
+
+function responder200(resposta, arquivo, tipoDeMidia) {
+		resposta.statusCode = 200;
+		resposta.setHeader("Content-Type", tipoDeMidia);
+		resposta.end(arquivo);
+}
+
+function responder404(resposta, caminho) {
+	resposta.statusCode = 404;
+	resposta.setHeader("Content-Type", "text/plain");
+	resposta.end(caminho);
+}
+
+function responder405(resposta, metodo) {
+	resposta.statusCode = 405;
+	resposta.setHeader("Content-Type", "text/plain");
+	resposta.end(metodo);
+}
+
+function responderComoErroDoServidor(resposta) {
+	resposta.statusCode = 500;
+	resposta.end();
+}
 
 var tiposDeMidiaPorExtensao = {
 	"js": "application/javascript",
@@ -34,38 +80,6 @@ var tiposDeMidiaPorExtensao = {
 	"png": "image/png"
 }
 
-function responderComoArquivo(resposta, caminho) {
-	sistemaDeArquivos.readFile(caminho, function (erro, arquivo) {
-		if (erro) {
-			responderComoErroDoServidor();
-		} else {
-			var extensao = caminho.split(".").pop();
-			var tipoDeMidia = tiposDeMidiaPorExtensao[extensao];
-			if (tipoDeMidia == null) {
-				tipoDeMidia = "text/plain";
-			}
-			resposta.writeHeader(200, {
-				"Content-Type": tipoDeMidia
-			});
-			resposta.end(arquivo);
-		}
-	});
-}
-
-function responderComoNaoEncontrado(resposta, caminho) {
-	resposta.writeHeader(404, {
-		"Content-Type": "text/plain"
-	});
-	resposta.end(caminho);
-}
-
-function responderComoMetodoNaoPermitido(resposta) {
-	resposta.statusCode = 405;
-	resposta.end();
-}
-
-function responderComoErroDoServidor(resposta) {
-	resposta.statusCode = 500;
-	resposta.end();
-}
-
+http.createServer(function (requisicao, resposta) {
+	responderRequisicao(requisicao, resposta);
+}).listen(7000);
